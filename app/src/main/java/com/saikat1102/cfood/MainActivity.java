185dd2da99ac88f91b93dd678523a8c078b7ab47
shohag1102 <u1904088@student.cuet.ac.bd>
2023-01-02 21:01:22 +0600

    First two screens done

---
+++ app/src/main/java/com/saikat1102/cfood/MainActivity.java
@@ -0,0 +1,53 @@
+package com.saikat1102.cfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar progress_horizontal;
+    private int progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                splash();
+                mainMenu();
+            }
+        });
+        thread.start();
+
+    }
+
+    private void mainMenu() {
+        Intent intent = new Intent(MainActivity.this, MainMenu.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void splash() {
+        for(progress = 10; progress<=100; progress+=10){
+            try {
+                Thread.sleep(300);
+                progress_horizontal.setProgress(progress);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
+    public void init(){
+        progress_horizontal = findViewById(R.id.progress_horizontal);
+    }
+
+}
