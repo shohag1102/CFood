04f93d3b962cfdc149d1f92bdc86bd3de6eddbd7
shohag1102 <u1904088@student.cuet.ac.bd>
2023-01-13 14:51:09 +0600

    logo changed

--- app/src/main/java/com/saikat1102/cfood/MainActivity.java
+++ app/src/main/java/com/saikat1102/cfood/MainActivity.java
@@ -1,17 +1,29 @@
 package com.saikat1102.cfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar progress_horizontal;
-
-    private int progress;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseAuth Fauth;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,40 +31,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
-        Thread thread = new Thread(new Runnable() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                splash();
+
                 mainMenu();
+
             }
-        });
-        thread.start();
+        }, 300);
 
 
 
     }
 
     private void mainMenu() {
-        Intent intent = new Intent(MainActivity.this, MainMenu.class);
-        startActivity(intent);
-        finish();
-    }
 
-    private void splash() {
-        for(progress = 10; progress<=100; progress+=10){
-            try {
-                Thread.sleep(300);
-                progress_horizontal.setProgress(progress);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Fauth = FirebaseAuth.getInstance();
+        if(Fauth.getCurrentUser()!=null){
+            if(Fauth.getCurrentUser().isEmailVerified()){
+                Fauth=FirebaseAuth.getInstance();
+
+                databaseReference = FirebaseDatabase.getInstance().getReference("User").child(FirebaseAuth.getInstance().getUid()+"/Role");
+                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String role = snapshot.getValue(String.class);
+                        if(role.equals("Chef")){
+                            startActivity(new Intent(MainActivity.this,ChefFoodPanel_BottomNavigation.class));
+                            finish();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MainActivity.this,error.getMessage(),Toast.LENGTH_LONG).show();
+
+                    }
+                });
             }
+            else{
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Check Whether You Have Verified Your Detail , Otherwise Please Verify");
+                builder.setCancelable(false);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                Fauth.signOut();
+            }
+        }
+        else{
+            Intent intent = new Intent(MainActivity.this, MainMenu.class);
+            startActivity(intent);
+            finish();
         }
     }
 
 
 
+
     public void init(){
-        progress_horizontal = findViewById(R.id.progress_horizontal);
+
     }
 
 }
