fec4442f5803e3a586b04e246ab74812452bc76d
shohag1102 <u1904088@student.cuet.ac.bd>
2023-01-16 20:53:17 +0600

    Bottom Navigation Bar From Scratch With Fragments done

--- app/src/main/java/com/saikat1102/cfood/ChefFoodPanel_BottomNavigation.java
+++ app/src/main/java/com/saikat1102/cfood/ChefFoodPanel_BottomNavigation.java
@@ -1,14 +1,64 @@
 package com.saikat1102.cfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.saikat1102.cfood.chefFoodPanel.ChefHomeFragment;
+import com.saikat1102.cfood.chefFoodPanel.ChefOrderFragment;
+import com.saikat1102.cfood.chefFoodPanel.ChefPendingOrderFragment;
+import com.saikat1102.cfood.chefFoodPanel.ChefProfileFragment;
 
 public class ChefFoodPanel_BottomNavigation extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_food_panel_bottom_navigation);
+        commence();
+
+        implementBottomNavigation();
+        replaceFragment(new ChefHomeFragment());
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,fragment).commit();
+    }
+
+    private void implementBottomNavigation() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.chefHome:
+                        replaceFragment(new ChefHomeFragment());
+                        return true;
+                    case R.id.PendingOrders:
+                        replaceFragment(new ChefPendingOrderFragment());
+                        return true;
+                    case R.id.Orders:
+                        replaceFragment(new ChefOrderFragment());
+                        return true;
+                    case R.id.chefProfile:
+                        replaceFragment(new ChefProfileFragment());
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+
+    private void commence() {
+        bottomNavigationView = findViewById(R.id.chef_bottom_navigation);
     }
 }
